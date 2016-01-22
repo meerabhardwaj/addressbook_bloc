261e222b2ce2fd28970b508ce71f96618624b8a4
Meera Bhardwaj <meerabhardwaj@gmail.com>
2016-01-21 17:36:59 -0800

    did 19 homework

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,24 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "deletes only entire entry using name, number or email" do
+      book = AddressBook.new
+      book.add_entry('Meera Bhardwaj','112.123.1234', 'meera@amazing.org')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Ada Lovelace')
+    end
+
+  end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
